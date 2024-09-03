7a87944e8ca02ed49e6cdabc3d421c6e9baf7cdf
AjiriOgeh <151365920+AjiriOgeh@users.noreply.github.com>
2024-09-03 10:36:06 +0100

    controller

--- src/main/java/com/example/hello_application/UserController.java
+++ src/main/java/com/example/hello_application/UserController.java
@@ -1,7 +1,15 @@
 package com.example.hello_application;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping()
+@RestController
 public class UserController {
+
+    @GetMapping("/api/v1/printhello")
+    public String printHello() {
+        return "Hello World";
+    }
 }
